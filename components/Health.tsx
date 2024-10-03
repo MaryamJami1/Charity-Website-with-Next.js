8a889223e20b5ced2a834fbcd908dda730cf0bd8
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-04 02:33:36 +0500

    project complete

--- components/Health.tsx
+++ components/Health.tsx
@@ -16,7 +16,7 @@ function Health() {
                     We deliver long-term <br /> international development <br /> programs, including <br /> education, nutrition, and <br /> health care.
                 </h2>
                 <p className="py-4 font-serif text-[#4c4c4ca0]">
-                    I also believe it's important for every member to be involved <br /> and invested in our company and this is one way to do so <br /> crank this out products need full resourcing and support from <br /> a cross-functional team.
+                    I also believe its important for every member to be involved <br /> and invested in our company and this is one way to do so <br /> crank this out products need full resourcing and support from <br /> a cross-functional team.
                 </p>
                 <div className="font-medium">
                     <button className="mt-6 bg-[#ff9400] text-white rounded w-[11rem] h-[3rem] hover:bg-orange-500 transform transition-all duration-300 hover:scale-105 hover:-translate-y-2 hover:shadow-2xl">
--- components/iframe.tsx
+++ components/iframe.tsx
@@ -10,7 +10,7 @@ return(
             We deliver long-term <br /> international development <br /> programs, including <br /> education, nutrition, and <br /> health care.
         </h2>
         <p className="py-4 md:py-8 font-serif text-[#4c4c4ca0]">
-            I also believe it's important for every member to be involved <br /> and invested in our company and this is one way to do so <br /> crank this out products need full resourcing and support from <br /> a cross-functional team.
+            I also believe its important for every member to be involved <br /> and invested in our company and this is one way to do so <br /> crank this out products need full resourcing and support from <br /> a cross-functional team.
         </p>
         <div className="font-medium">
             <button className="mt-4 md:mt-6 bg-[#ff9400] text-white rounded w-full md:w-[11rem] h-[3rem] hover:bg-orange-500 transform transition-all duration-300 hover:scale-105 hover:-translate-y-2 hover:shadow-2xl">
